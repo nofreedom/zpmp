19f2657d654b5e40dc445d647b763a9eb73fec30
super nofreedom <supernofreedom@gmail.com>
2011-09-14 16:04:05 +0800

    correct wave

--- tx.py
+++ tx.py
@@ -8,10 +8,13 @@ fs  = 32000000  #Sample rate at   32MHz
 tc  = 1.0/2000000   #chip length in seconds
 fc  = 1.0/(2*tc)    #the frequency of half-sine wave of chip
 pi  = math.pi
-number_sample_per_chip = fs/2000000
+number_sample_per_chip = fs/2000000 
+x_axis = np.arange(0, number_sample_per_chip)
 
-samples = np.arange(0, number_sample_per_chip)
-baseband_wave = np.sin(samples*fc/(2*pi))
+def baseband_wave(x_axis):
+    half_sine_wave = np.sin(x_axis*2*pi/number_sample_per_chip)
+    return half_sine_wave
+
 plt.figure()
-plt.plot(samples,baseband_wave)
+plt.plot(x_axis,baseband_wave(x_axis))
 plt.show()
